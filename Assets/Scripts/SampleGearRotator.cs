892a2e96a474304f9410531421b2b0b2fd63af47
StefanoVaccaro <stefanovaccaro995@gmail.com>
2020-02-02 02:52:36 +0100

    - push nuova versione

---
+++ Assets/Scripts/SampleGearRotator.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SampleGearRotator : MonoBehaviour
+{
+    public float rotationSpeed;
+    
+
+    void Update()
+    {
+        this.transform.Rotate(0f, 0f, rotationSpeed);
+    }
+}
---
+++ Assets/Scripts/SwapSprite.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SwapSprite : MonoBehaviour
+{
+    public Sprite spriteOn;
+    public Sprite spriteOff;
+    public float swapSpriteInterval = 2f;
+
+    private Image currentSprite;
+    
+
+
+    void Start()
+    {
+        currentSprite = this.GetComponent<Image>();
+        StartCoroutine(SwapSpriteContent());
+    }
+
+
+    private IEnumerator SwapSpriteContent()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(swapSpriteInterval);
+            if (currentSprite.sprite == spriteOff) currentSprite.sprite = spriteOn;
+            else if (currentSprite.sprite == spriteOn) currentSprite.sprite = spriteOff;
+        }
+    }
+}
